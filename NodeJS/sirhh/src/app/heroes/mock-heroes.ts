3bde04d87ea2c35b533fd6750528f2034fb21099
walidbenf <45196746+walidbenf@users.noreply.github.com>
2023-03-25 10:12:36 +0100

    Add files via upload

---
+++ NodeJS/sirhh/src/app/heroes/mock-heroes.ts
@@ -0,0 +1,14 @@
+import {
+  Hero
+} from './hero';
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Sangoku', url: 'https://vignette.wikia.nocookie.net/dragonball/images/8/8d/Goku2.jpg.png/revision/latest/scale-to-width-down/268?cb=20180611154632&path-prefix=fr' },
+  { id: 12, name: 'Sangohan', url: 'http://planetedragonball.free.fr/IMAGES/Sangohan5%20SSJ.jpg' },
+  { id: 13, name: 'Sangoten', url: 'http://saiyansworld1.free.fr/images/gentils/gotendbz/sangoten-33.jpg' },
+  { id: 14, name: 'Trunks', url: 'https://i.pinimg.com/originals/dc/7e/a6/dc7ea6cf222d87a19144d7329e7a1ab8.jpg' },
+  { id: 15, name: 'Bulma', url: 'https://vignette.wikia.nocookie.net/dragonuniverse/images/c/cd/Bulma23rdTenkaichiBudokai.png/revision/latest?cb=20160205025729' },
+  { id: 16, name: 'Cell', url: 'https://vignette.wikia.nocookie.net/dragonball/images/c/cc/Cell_perfect_on_dokkan_battle.png/revision/latest?cb=20170726201518&path-prefix=fr' },
+  { id: 17, name: 'Yamcha', url: 'https://i.pinimg.com/originals/08/3c/df/083cdff38fb7bbfaae0e31fb0253bbb1.jpg' },
+  { id: 18, name: 'Tenshinhan', url: 'https://vignette.wikia.nocookie.net/dragon-and-incredible-adventures/images/e/e3/Ten_shin_han_by_maffo1989-d68ymq5.png/revision/latest?cb=20170731161805' },
+  { id: 19, name: 'Broly', url: 'https://s3-eu-central-1.amazonaws.com/play-sick/uploads/2018/10/Broly-Film-T2-01-655x300.png' },
+]
